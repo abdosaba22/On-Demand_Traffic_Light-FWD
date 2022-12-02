63ca78e7fba502ec7b94cbf8d045580b9b1dc16d
Abdalrahman Sabaa <99027893+abdosaba22@users.noreply.github.com>
2022-12-02 23:50:26 +0200

    Update header.h

--- On-Demand-Traafic-Light/Traffic_Light_FWD_Code/Traffic_Light_FWD/UTIL/header.h
+++ On-Demand-Traafic-Light/Traffic_Light_FWD_Code/Traffic_Light_FWD/UTIL/header.h
@@ -8,17 +8,14 @@
 #ifndef HEADER_H_ 
 #define HEADER_H_
  
-#include <avr/io.h>   // include all register addresses
+//#include <avr/io.h>   // include all register addresses
 #define F_CPU 8000000UL // set the frequency 
 #include <util/delay.h> // this header include all delay function 
 #include <avr/interrupt.h> // this header is necessary when you deal with interrupt
-//#include "FreeRTOS.h"
-//#include "task.h"
-//#include "LCD.h"
 
-//typedef unsigned short uint16_t;
-//typedef unsigned char uint8_t;
-//typedef unsigned long uint32_t;
+typedef unsigned short uint16_t;
+typedef unsigned char uint8_t;
+typedef unsigned long uint32_t;
 
 
 #define SETBIT(REG,BIT) (REG |= 1<<BIT)
